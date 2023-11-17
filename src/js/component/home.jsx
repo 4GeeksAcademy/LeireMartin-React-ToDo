42cd8ee67d0c1b67bcba17064ef790082cfe7fb9
Leire M-Berdinos <134960908+leimber@users.noreply.github.com>
2023-11-17 19:59:54 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, {useState}   from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+
 
 //create your first component
-const Home = () => {
+const List = () => {
+	const [inputValue, setinputValue] = useState("");
+	const [toDos, settoDos] = useState([])
+	const [hoveredIndex, setHoveredIndex] = useState(null)
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>to Dos</h1>
+			<ul>
+				<li>
+					<input
+						 type="text" 
+						 onChange={(e) => setinputValue(e.target.value)}
+						 value={inputValue}
+						 onKeyPress={(e) => {
+                            if (e.key === "Enter") {
+                                settoDos(prevToDos => [...prevToDos, inputValue]);
+                                setinputValue("");
+                            }
+                        }}
+						placeholder={toDos.length === 0 ? "Nothing for now. Something new?" : "More to do?"}></input>
+				</li>
+				
+				{toDos.map((item, index) => (
+					<li className="trash"
+						key={index}
+						onMouseEnter={() => setHoveredIndex(index)}
+                        onMouseLeave={() => setHoveredIndex(null)}>
+						
+						<span className="does">{item}</span>
+						
+						{hoveredIndex === index && (
+                            <FontAwesomeIcon
+                                icon={faTrash}
+                                onClick={() => 
+                                    settoDos(prevToDos => 
+                                        prevToDos.filter(
+                                            (t, currentIndex) => index !== currentIndex
+                                        )
+                                    )
+                                }
+                            />
+                        )}
+						 
+					</li>
+					
+				))}
+			</ul>
+			<div>{toDos.length === 0 ? "No tasks! Free!" : `${toDos.length} tasks to go`}</div>
 		</div>
 	);
 };
 
-export default Home;
+export default List;
--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,9 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
+
 //import your own components
-import Home from "./component/home.jsx";
+import List from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<List />, document.querySelector("#app"));
